3dbea09252b1f897470f575f2b12bacc50a1deff
unknown <laura.m.stevens@gmail.com>
2018-06-15 20:02:16 -0500

    assignment 6 bloc jams react album complete.

--- src/components/Album.js
+++ src/components/Album.js
@@ -32,6 +32,13 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
+          {this.state.album.songs.map( (song, index) =>
+            <tr className="song" key={index}>
+              <td className="song-number">{song.number}{index + 1}</td>
+              <td className="song-title">{song.title}</td>
+              <td className="song-duration">{song.duration}</td>
+            </tr>
+          )}
           </tbody>
         </table>
       </section>
